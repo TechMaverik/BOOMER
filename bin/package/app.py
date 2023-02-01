e237c6776cb08f5a8789d357576d1f49f25b37dd
1367 <abhijith.saji@trenser.com>
2023-02-01 10:10:39 +0530

    requirement windows implementation

---
+++ bin/package/app.py
@@ -0,0 +1,7 @@
+class app():
+    def __init__(self):
+        pass
+    def run(self):
+        pass
+    def close(self):
+        pass
---
+++ bin/package/main_window.py
@@ -0,0 +1,10 @@
+class main_window():
+    def __init__(self):
+        pass
+    def show_window(self):
+        pass
+    def hide_window(self):
+        pass
+    def get_window(self):
+        pass
+    
